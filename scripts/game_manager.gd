979efd4e1021286c4e6d9c75c7270985178e689a
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2024-10-15 21:26:22 -0400

    add spawn things and players colors

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -15,6 +15,14 @@ func on_game_started(player_data_array : Array):
 	for i in range(len(player_data_array)):
 		var player_instance : Player = player_prefab.instantiate()
 		
+		#always do initialization after add child to avoid uninitialiazation porblems
+		
+		world.add_child(player_instance)
+		player_instance.position = spawn_points.get_child(i).position
+		player_instance.construct(player_data_array[i])
+		
+	
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
