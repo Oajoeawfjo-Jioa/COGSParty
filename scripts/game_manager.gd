93d500286d2657f70dbd5a8a97f83abd9c83bac0
Atlinx <atlinuxtm@gmail.com>
2024-12-03 21:08:30 -0500

    Add basic map and movement

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,11 +4,14 @@ extends Node
 @export var player_prefab : PackedScene
 @export var spawn_points : Node2D
 @export var world : Node2D
+@export var collision_baker: TilemapCollisionBaker
+@export var tilemap_layer: TileMapLayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#attach listener to that signal
+	# Attach listener to that signal
 	minigame_manager.game_started.connect(on_game_started)
+	collision_baker.bake_tilemaplayer(tilemap_layer)
+
 
 func on_game_started(player_data_array : Array):
 	print(player_data_array)
@@ -20,10 +23,7 @@ func on_game_started(player_data_array : Array):
 		world.add_child(player_instance)
 		player_instance.position = spawn_points.get_child(i).position
 		player_instance.construct(player_data_array[i])
-		
-	
-		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,36 +1,45 @@
 extends RigidBody2D 
-
-#start using script as a class wtfff??
 class_name Player
 
 @export var acceleration = 400.0
 @export var speed = 800
+@export var jump_amount = 1000
 @export var color_rect : ColorRect
+@export var animated_sprite: AnimatedSprite2D
+@export var ground_cast: ShapeCast2D
 var device_ID : int = -1
-var screen_size = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	screen_size = get_viewport_rect().size
-	print(screen_size)
-	position.x = 0
-	position.y = 0
-	
-	
+enum State {
+	IDLE,
+	RUN,
+	JUMP,
+	FALL
+}
+
+var state: State = State.IDLE
+var is_on_floor: bool = false
+
 
 func construct(player_data : MiniGameManager.PlayerData):
 	if (player_data.index < len(Input.get_connected_joypads())):
 		device_ID = Input.get_connected_joypads()[player_data.index]
 	color_rect.modulate = player_data.color
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
+	is_on_floor = ground_cast.is_colliding()
+	
 	if (device_ID < 0):
 		return
+	
 	var direction = Input.get_joy_axis(device_ID, JOY_AXIS_LEFT_X) * acceleration
 	
-	if (linear_velocity.length() <= speed):
-		apply_impulse(Vector2(direction,0), Vector2.ZERO)
+	# Jumping
+	if Input.is_joy_button_pressed(device_ID, JOY_BUTTON_LEFT_SHOULDER):
+		apply_impulse(Vector2.UP * jump_amount, Vector2.ZERO)
 	
-	
-	
+	# Horizontal movement
+	var curr_speed = abs(linear_velocity.x)
+	if (curr_speed <= speed):
+		var scaling = (speed - curr_speed) / speed
+		apply_impulse(Vector2(direction * scaling, 0), Vector2.ZERO)
